dc4e80b82ca8b4437f0d5bc99b8b2172ab467926
savar <savar@oasis.localdomain>
2023-05-14 23:31:49 +0530

    added gruv

---
+++ colorschemes/gruvbox.c
@@ -0,0 +1,27 @@
+	/* 8 normal colors */
+	"#292929",
+	"#fb4934",
+	"#b8bb26",
+	"#d79921",
+	"#458588",
+	"#b4bbc8",
+	"#82b3a8",
+	"#ebdbb2",
+
+	/* 8 bright colors */
+	"#292929",
+	"#fb4934",
+	"#b8bb26",
+	"#d79921",
+	"#458588",
+	"#b4bbc8",
+	"#82b3a8",
+	"#ebdbb2",
+
+	[255] = 0,
+
+	/* more colors can be added after 255 to use with DefaultXX */
+	"#bdae93", // cursor
+	"#505050",
+	"gray90", /* default foreground colour */
+	"#282828", /* default background colour */
